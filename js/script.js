f0a5f77f738b519d177cb2e9fe1f973d8521405e
Pablo <cruzpablomartin@gmail.com>
2021-01-14 19:01:13 -0300

    Refactoring. Application of the DRY Principle.

--- js/script.js
+++ js/script.js
@@ -2,69 +2,66 @@
 
 // IMPORTANTE: las variables secretNumber, score y highscore se denominan 'variables de estado', ya que éstas variables son parte del 'estado de la aplicación', por eso SIEMPRE es bueno declarar variables que contengan los valores que sean relevantes de la aplicación para que siempre esten disponibles en nuestro código y no que dependan solo del DOM.
 
+const displayMessage = message => document.querySelector('.message').textContent = message;
+const displaySecretNumber = nro => document.querySelector('.number').textContent = nro;
+const hideSecretNumber = () => document.querySelector('.number').textContent = '?';
+const setScore = score => document.querySelector('.score').textContent = score;
+const setHighscore = value => document.querySelector('.highscore').textContent = value;
+const setInputValue = value => document.querySelector('.guess').value = value;
+const getInputValue = () => document.querySelector('.guess').value;
+const changeBoxWidth = value => document.querySelector('.number').style.width = value;
+const changeBackgroundColor = color => document.querySelector('body').style.backgroundColor = color;
 
-let secretNumber = Math.trunc(Math.random() * 20) + 1; // Número aleatorio entre 1 y 20.
-let score = 20;
-let highscore = 0;
+let secretNumber, score, highscore;
 
+secretNumber = Math.trunc(Math.random() * 20) + 1; // Número aleatorio entre 1 y 20.
+score = 20;
+highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
 
-   // Guardo el valor de entrada convertido a tipo de dato number. 
-   const guess = Number(document.querySelector('.guess').value);
+   const guess = Number(getInputValue());
 
    // Cuando no se ingresa un valor de entrada.
    if (!guess) {
-      document.querySelector('.message').textContent = '⛔ No Number!';
+      displayMessage('⛔ No Number!');
 
       // Cuando se adivina el número secreto. 
    } else if (guess === secretNumber) {
       if (score > highscore) {
          highscore = score;
-         document.querySelector('.highscore').textContent = highscore;
+         setHighscore(highscore);
       }
 
-      document.querySelector('.message').textContent = '🎉 Correct Number!';
-      document.querySelector('body').style.backgroundColor = '#60b347';
-      document.querySelector('.number').style.width = '30rem';
-      document.querySelector('.number').textContent = secretNumber;
-      document.querySelector('.score').textContent = score;
+      displayMessage('🎉 Correct Number!');
+      changeBackgroundColor('#60b347');
+      changeBoxWidth('30rem');
+      displaySecretNumber(secretNumber);
+      setScore(score);
 
-      // Cuando el valor de entrada es mas alto que el número secreto.  
-   } else if (guess > secretNumber) {
+      // Cuando no se adivina el número secreto.  
+   } else if (guess !== secretNumber) {
       if (score > 1) {
          score--;
-         document.querySelector('.message').textContent = '📈 Too high!'; // Muy alto.
-         document.querySelector('.score').textContent = score;
+         displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+         setScore(score);
       } else {
-         document.querySelector('.message').textContent = '💥 You lost the game!'
-         document.querySelector('.score').textContent = 0;
-      }
-
-      // Cuando el valor de entrada es mas bajo que el número secreto.  
-   } else if (guess < secretNumber) {
-      if (score > 1) {
-         score--;
-         document.querySelector('.message').textContent = '📉 Too low!'; // Muy bajo.
-         document.querySelector('.score').textContent = score;
-      } else {
-         document.querySelector('.message').textContent = '💥 You lost the game!'
-         document.querySelector('.score').textContent = 0;
+         displayMessage('💥 You lost the game!');
+         setScore(0);
       }
    }
 });
-
 
 document.querySelector('.again').addEventListener('click', function () {
    secretNumber = Math.trunc(Math.random() * 20) + 1;
    score = 20;
 
-   document.querySelector('.message').textContent = 'Start guessing...';
-   document.querySelector('body').style.backgroundColor = '#222';
-   document.querySelector('.number').style.width = '15rem';
-   document.querySelector('.number').textContent = '?';
-   document.querySelector('.guess').value = '';
-   document.querySelector('.score').textContent = score;
+   displayMessage('Start guessing...');
+   changeBackgroundColor('#222');
+   changeBoxWidth('15rem');
+   hideSecretNumber();
+   setInputValue('');
+   setScore(score);
 });
 
 
